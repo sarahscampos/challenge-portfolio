4695a123b5f5dafde16705393090d0238b3887fb
sarah-campos <sarahstephanycampos@gmail.com>
2024-10-01 23:15:49 -0300

    feat: envio de formulario de contato

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+const nomeInput = document.querySelector('input[name="name"]');
+const emailInput = document.querySelector('input[name="email"]');
+const assuntoInput = document.querySelector('input[name="assunto"]');
+const mensagemTextArea = document.querySelector('textarea[name="mensagem"]');
+
+const botaoSubmit = document.querySelector('input[type="submit"]');
+function verificarCamposPreenchidos() {
+  if (nomeInput.value && emailInput.value && assuntoInput.value && mensagemTextArea.value) {
+    botaoSubmit.removeAttribute('disabled'); // Habilita o botão
+  } else {
+    botaoSubmit.setAttribute('disabled', 'disabled'); // Desabilita o botão
+  }
+}
+
+
+nomeInput.addEventListener('input', verificarCamposPreenchidos);
+emailInput.addEventListener('input', verificarCamposPreenchidos);
+assuntoInput.addEventListener('input', verificarCamposPreenchidos);
+mensagemTextArea.addEventListener('input', verificarCamposPreenchidos);
+
+
+verificarCamposPreenchidos();
